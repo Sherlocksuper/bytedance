c05b0e171e4115e1b4732b9007dac6f963e6b5d1
陈基伟 <2097676238@qq.com>
2023-08-10 22:25:10 +0800

    dialog基本功能完成

--- src/App.vue
+++ src/App.vue
@@ -21,5 +21,8 @@ export default {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+
 }
+
+
 </style>
--- src/sub_module_items/item_interface_list/item_interface_detail_page/detail_router_view/test.vue
+++ src/sub_module_items/item_interface_list/item_interface_detail_page/detail_router_view/test.vue
@@ -1,21 +1,12 @@
 <template>
-  <el-input
-    v-model="textarea1"
-    autosize
-    type="textarea"
-    placeholder="Please input"
-  />
-  <div style="margin: 20px 0" />
-  <el-input
-    v-model="textarea2"
-    :autosize="{ minRows: 2, maxRows: 4 }"
-    type="textarea"
-    placeholder="Please input"
-  />
+
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
-const textarea1 = ref('')
-const textarea2 = ref('')
+
+const num = ref(1)
+const handleChange = (value: number) => {
+  console.log(value)
+}
 </script>
